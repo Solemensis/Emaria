67b75395ef33c582d59b83b1743d43c3b2b5e3e1
Ulaş Saçlı <ulassacli@outlook.com>
2022-10-14 21:51:19 +0300

    pd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,8 +15,4 @@ export default defineNuxtConfig({
     dirs: ["stores"],
   },
   target: "static",
-  // publicRuntimeConfig: {
-  //   SUPABASE_URL: process.env.SUPABASE_URL,
-  //   SUPABASE_KEY: process.env.SUPABASE_KEY,
-  // },
 });
