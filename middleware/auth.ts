6fe82602fd01ed8fc3108ca3b62e833be1f8c970
Ulaş Saçlı <ulassacli@outlook.com>
2022-11-30 16:58:34 +0300

    upz

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
-  const user = useSupabaseUser();
+  // const user = useSupabaseUser();
   if (user.value) {
     return navigateTo("/");
   }
