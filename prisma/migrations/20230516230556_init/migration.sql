6d422877e8fd5e307a7168783e0031d2b8f94b23
Ulaş Saçlı <ulassacli@outlook.com>
2023-05-17 02:10:37 +0300

    shadow db added
    shadow db added

---
+++ prisma/migrations/20230516230556_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `amount` on the `Carts` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Carts" DROP COLUMN "amount";
