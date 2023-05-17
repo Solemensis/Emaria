0eb11e0dbf2afa4a939e5e525da886fa12817564
Ulaş Saçlı <ulassacli@outlook.com>
2023-05-18 00:15:21 +0300

    migration upd
    migration upd

--- prisma/migrations/20230516230556_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `amount` on the `Carts` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Carts" DROP COLUMN "amount";
--- prisma/migrations/20230517001330_preview_feature/migration.sql
+++ prisma/migrations/20230517001330_preview_feature/migration.sql
@@ -3,7 +3,6 @@ CREATE TABLE "Carts" (
     "id" SERIAL NOT NULL,
     "user_id" TEXT NOT NULL,
     "cart" JSONB NOT NULL,
-    "amount" INTEGER NOT NULL,
 
     CONSTRAINT "Carts_pkey" PRIMARY KEY ("id")
 );
